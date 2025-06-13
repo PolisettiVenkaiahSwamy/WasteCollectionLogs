aba007a002214bc3046459aef799aab06a7eaf3d
Swamy <2408530@cognizant.com>
2025-06-13 09:03:48 +0530

    Made changes in the repository

--- src/main/java/com/WasteWise/WasteCollectionLogs/Repository/WasteLogRepository.java
+++ src/main/java/com/WasteWise/WasteCollectionLogs/Repository/WasteLogRepository.java
@@ -14,10 +14,7 @@ public interface WasteLogRepository extends JpaRepository<WasteLog,Long>{
     
     List<WasteLog>findByZoneIdAndCollectionStartTimeBetween(String zoneId,LocalDateTime startDate,LocalDateTime endTime);
     
-//    List<WasteLog>findByCollectionStartTimeBetween(ZonedDateTime startDate, ZonedDateTime endTime);
     
     List<WasteLog> findByVehicleIdAndCollectionStartTimeBetween(String vehicleId, LocalDateTime startDateTime, LocalDateTime endDateTime);
     
-//    @Query("SELECT MAX(w.logId) FROM WasteLog w")
-//    String findMaxLogId(); // This will return "999" if "999" is the max, or "100" if "100" is max.
 }
